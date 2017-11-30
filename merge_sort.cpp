99346b1ce40ce0511210e4eb9a7d91c9ee7208c1
tskrzypiec <tomasz.t.skrzypiec@gmail.com>
2017-11-30 14:41:17 +0100

    ver.2

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -7,65 +7,109 @@
 
     using namespace std;
 
-    const int N = 10; // Liczebność zbioru.
+    const int ile_liczb = 10; // liczba sortowanych liczb
 
-    int d[N]; //tabliba z posortowanymi liczbami
-    int p[N]; //tablica pomocnicza, w której odkładamy scalane elementy dwóch zbiorów
+    int tablica_glowna[ile_liczb]; //tabliba z posortowanymi liczbami
+    int schowek[ile_liczb]; //tablica pomocnicza, w której odkładamy scalane elementy dwóch zbiorów
 
-// Procedura sortująca
-//--------------------
 
-    void MergeSort(int i_p, int i_k) //i_p czyli index pierwszego elementu zbioru sortowanego, i_k - index końcowego elementu zbioru
+    void MergeSort(int i_poczatek, int i_koniec) //"i_poczatek" czyli indeks pierwszego elementu zbioru sortowanego, "i_koniec" - indeks końcowego elementu zbioru
     {
-        int i_s; //index pierwszego elementu ze zbioru liczb prawej połówki
-        int i1; //index elementów lewego zbioru liczb tablicy d[]
-        int i2; //index elementów prawego zbioru liczb tablicy d[]
-        int i; //index elementów zbioru liczb tablicy p[]
+        int i_srodek; //indeks pierwszego elementu ze zbioru liczb prawej połówki
+        int i_lewy; //indeks elementów lewego zbioru liczb tablica_glowna
+        int i_prawy; //indeks elementów prawego zbioru liczb tablicy_glowna
+        int i_schowek; //indeks elementów zbioru liczb tablicy schowek
 
-        i_s = (i_p + i_k + 1) / 2; //
-        if(i_s - i_p > 1) MergeSort(i_p, i_s - 1);
-        if(i_k - i_s > 0) MergeSort(i_s, i_k);
-        i1 = i_p; i2 = i_s;
-        for(i = i_p; i <= i_k; i++)
-            p[i] = ((i1 == i_s) || ((i2 <= i_k) && (d[i1] > d[i2]))) ? d[i2++] : d[i1++];
-        for(i = i_p; i <= i_k; i++) d[i] = p[i];
+        i_srodek = (i_poczatek + i_koniec + 1) / 2; // wyznaczanie indeksu "srodek", który dzieli zbiór na dwie połówki
+
+        if(i_srodek - i_poczatek > 1)
+        {
+            MergeSort(i_poczatek, i_srodek - 1); // sprawdzamy czy lewa połówka zawiera więcej niż jeden element jak tak to rekurencyjnie sortujemy
+        }
+        if(i_koniec - i_srodek > 0) //sprawdzamy czy w prawej połówce jest chociaż jedna liczba
+        {
+            MergeSort(i_srodek, i_koniec); //sortujemy prawy podzbiór
+        }
+
+        i_lewy = i_poczatek; //przypisujemy indeks pierwszego elementu w lewym podzbiorze
+        i_prawy = i_srodek; //przypisujemy indeks pierwszego elementu w prawym podzbiorze
+
+        for(i_schowek = i_poczatek; i_schowek <= i_koniec; i_schowek++)
+        {
+            //Porównujemy ze sobą najmniejsze elementy scalanych zbiorów. Ponieważ zbiory te są już uporządkowane, to najmniejszymi elementami będą zawsze ich pierwsze elementy.
+
+            // dopóki żaden z podzbiorów lewy i prawy nie jest wyczerpany, porównujemy kolejne elementy z tych podzbiorów wg indeksów i_lewy i i_prawy
+            if (((i_lewy == i_srodek) || ((i_prawy <= i_koniec) && (tablica_glowna[i_lewy] > tablica_glowna[i_prawy]))))
+            {
+
+                schowek[i_schowek] = tablica_glowna[i_prawy++];
+            }
+            else
+            {
+                schowek[i_schowek] = tablica_glowna[i_lewy++];
+            }
+
+        }
+
+        for(i_schowek = i_poczatek; i_schowek <= i_koniec; i_schowek++) //zapisujemy "tablice_glowna" liczbami posortowanymi umieszczonymi w "schowek"
+        {
+            tablica_glowna[i_schowek] = schowek[i_schowek];
+        }
     }
 
-// Program główny
-//---------------
+
+//int main()
+//{
+   /* int i;
+
+    cout <<"Przed sortowaniem:\n\n";
+
+    srand((unsigned)time(NULL));
+
+    for(i = 0; i < ile_liczb; i++) tablica_glowna[i] = rand() % 100;
+    for(i = 0; i < ile_liczb; i++) cout << setw(4) << tablica_glowna[i];
+    cout << endl;
+
+    MergeSort(0,ile_liczb-1);
+
+    cout << "Po sortowaniu:\n\n";
+    for(i = 0; i < ile_liczb; i++) cout << setw(4) << tablica_glowna[i];
+    cout << endl;
+    return 0;
+}    */
 
     int main()
     {
         int i;
         int liczba=0;
 
-       // cout << "Przed sortowaniem:\n\n";
+        // cout << "Przed sortowaniem:\n\n";
 
         for (int j =0; j<5; j++)
 
         {
-            cout<< "wpisz" <<j+1<<"liczbe i nacisnij enter"<<endl;
-            cin>>i;
+            cout<< "wpisz " << j+1 <<" liczbe i nacisnij enter"<<endl;
+            cin>>liczba;
 
         }
 
-        d[i]=i;
+        tablica_glowna[i]=liczba;
 
 
-       for(i = 0; i < 5; i++)
-       {
-           cout <<d[i] <<endl;
-       }
+        for(i = 0; i < 5; i++)
+        {
+            cout <<tablica_glowna[i] <<endl;
+        }
 
 // Sortujemy
 
-        MergeSort(0,N-1); //indeks początkowy zbioru liczb do posortowania czyli "0" i końcowy czyli N-1 ( N czyli liczebność zbioru minus 1, bo indeksy liczymy od zera).
+        MergeSort(0,ile_liczb-1); //indeks początkowy zbioru liczb do posortowania czyli "0" i końcowy czyli N-1 ( N czyli liczebność zbioru minus 1, bo indeksy liczymy od zera).
 
 
 // Wyświetlamy wynik sortowania
 
         cout << "Po sortowaniu:\n\n";
-        for(i = 0; i < N; i++) cout << setw(4) << d[i];
+        for(i = 0; i < ile_liczb; i++) cout << setw(4) << tablica_glowna[i];
         cout << endl;
         return 0;
     }
