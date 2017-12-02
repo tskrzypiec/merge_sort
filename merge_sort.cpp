4e096d0cd729f195c2ca4671bdce2985a1e78ade
tskrzypiec <tomasz.t.skrzypiec@gmail.com>
2017-12-02 23:12:40 +0100

    ver.4

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -7,18 +7,18 @@
 
     using namespace std;
 
-    const int ile_liczb = 10; // liczba sortowanych liczb
+    const int ile_liczb = 5; // liczba sortowanych liczb
 
-    int tablica_glowna[ile_liczb]; //tabliba z posortowanymi liczbami
-    int schowek[ile_liczb]; //tablica pomocnicza, w której odkładamy scalane elementy dwóch zbiorów
+    int tablica_glowna[ile_liczb]; // tabliba z posortowanymi liczbami
+    int schowek[ile_liczb]; // tablica pomocnicza, w której odkładamy scalane elementy dwóch zbiorów
 
 
-    void MergeSort(int i_poczatek, int i_koniec) //"i_poczatek" czyli indeks pierwszego elementu zbioru sortowanego, "i_koniec" - indeks końcowego elementu zbioru
+    void MergeSort(int i_poczatek, int i_koniec) // "i_poczatek" - indeks pierwszego elementu zbioru sortowanego, "i_koniec" - indeks końcowego elementu zbioru
     {
-        int i_srodek; //indeks pierwszego elementu ze zbioru liczb prawej połówki
-        int i_lewy; //indeks elementów lewego zbioru liczb tablica_glowna
-        int i_prawy; //indeks elementów prawego zbioru liczb tablicy_glowna
-        int i_schowek; //indeks elementów zbioru liczb tablicy schowek
+        int i_srodek; // indeks pierwszego elementu ze zbioru liczb prawej połówki
+        int i_lewy; // indeks elementów lewego zbioru liczb tablica_glowna
+        int i_prawy; // indeks elementów prawego zbioru liczb tablicy_glowna
+        int i_schowek; // indeks elementów zbioru liczb tablicy schowek
 
         i_srodek = (i_poczatek + i_koniec + 1) / 2; // wyznaczanie indeksu "srodek", który dzieli zbiór na dwie połówki
 
@@ -26,15 +26,15 @@
         {
             MergeSort(i_poczatek, i_srodek - 1); // sprawdzamy czy lewa połówka zawiera więcej niż jeden element jak tak to rekurencyjnie sortujemy
         }
-        if(i_koniec - i_srodek > 0) //sprawdzamy czy w prawej połówce jest chociaż jedna liczba
+        if(i_koniec - i_srodek > 0) // sprawdzamy czy w prawej połówce jest chociaż jedna liczba
         {
-            MergeSort(i_srodek, i_koniec); //sortujemy prawy podzbiór
+            MergeSort(i_srodek, i_koniec); // sortujemy prawy podzbiór
         }
 
-        i_lewy = i_poczatek; //przypisujemy indeks pierwszego elementu w lewym podzbiorze
-        i_prawy = i_srodek; //przypisujemy indeks pierwszego elementu w prawym podzbiorze
+        i_lewy = i_poczatek; // przypisujemy indeks pierwszego elementu w lewym podzbiorze
+        i_prawy = i_srodek; // przypisujemy indeks pierwszego elementu w prawym podzbiorze
 
-        for(i_schowek = i_poczatek; i_schowek <= i_koniec; i_schowek++)//Porównujemy ze sobą najmniejsze elementy scalanych zbiorów. Ponieważ zbiory te są już uporządkowane, to najmniejszymi elementami będą zawsze ich pierwsze elementy.
+        for(i_schowek = i_poczatek; i_schowek <= i_koniec; i_schowek++)// porównujemy ze sobą najmniejsze czyli pierwsze elementy uporządkowanych scalanych zbiorów
         {
             // dopóki żaden z podzbiorów lewy i prawy nie jest wyczerpany, porównujemy kolejne elementy z tych podzbiorów wg indeksów i_lewy i i_prawy
             if (((i_lewy == i_srodek) || ((i_prawy <= i_koniec) && (tablica_glowna[i_lewy] > tablica_glowna[i_prawy]))))
@@ -49,7 +49,7 @@
 
         }
 
-        for(i_schowek = i_poczatek; i_schowek <= i_koniec; i_schowek++) //zapisujemy "tablice_glowna" liczbami posortowanymi umieszczonymi w "schowek"
+        for(i_schowek = i_poczatek; i_schowek <= i_koniec; i_schowek++) // zapisujemy "tablice_glowna" liczbami posortowanymi ze "schowek"
         {
             tablica_glowna[i_schowek] = schowek[i_schowek];
         }
@@ -76,43 +76,6 @@ int main()
 }
 
 
-   /* int main()
-    {
-        int i;
-        int liczba=0;
-
-        // cout << "Przed sortowaniem:\n\n";
-
-        for (int j =0; j<5; j++)
-
-        {
-            cout<< "wpisz " << j+1 <<" liczbe do posortowania i nacisnij enter"<<endl;
-            cin>>liczba;
-            tablica_glowna[j]=liczba;
-        }
-
-        cout<<"Twoje liczby to: "<<endl;
-
-
-        for(i = 0; i < 5; i++)
-        {
-            cout <<tablica_glowna[i];
-        }
-
-// Sortujemy
-
-        MergeSort(0,ile_liczb-1); //indeks początkowy zbioru liczb do posortowania czyli "0" i końcowy czyli N-1 ( N czyli liczebność zbioru minus 1, bo indeksy liczymy od zera).
-
-
-// Wyświetlamy wynik sortowania
-
-        cout << "Po sortowaniu:\n\n";
-        for(i = 0; i < ile_liczb; i++) cout << setw(4) << tablica_glowna[i];
-        cout << endl;
-        return 0;
-    }
-
-*/
 
 
 
