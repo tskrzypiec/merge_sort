c79cb104bc3b586b63ab9e24a6f559013d7dbebd
tskrzypiec <tomasz.t.skrzypiec@gmail.com>
2017-12-02 22:56:26 +0100

    ver.3

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -34,10 +34,8 @@
         i_lewy = i_poczatek; //przypisujemy indeks pierwszego elementu w lewym podzbiorze
         i_prawy = i_srodek; //przypisujemy indeks pierwszego elementu w prawym podzbiorze
 
-        for(i_schowek = i_poczatek; i_schowek <= i_koniec; i_schowek++)
+        for(i_schowek = i_poczatek; i_schowek <= i_koniec; i_schowek++)//Porównujemy ze sobą najmniejsze elementy scalanych zbiorów. Ponieważ zbiory te są już uporządkowane, to najmniejszymi elementami będą zawsze ich pierwsze elementy.
         {
-            //Porównujemy ze sobą najmniejsze elementy scalanych zbiorów. Ponieważ zbiory te są już uporządkowane, to najmniejszymi elementami będą zawsze ich pierwsze elementy.
-
             // dopóki żaden z podzbiorów lewy i prawy nie jest wyczerpany, porównujemy kolejne elementy z tych podzbiorów wg indeksów i_lewy i i_prawy
             if (((i_lewy == i_srodek) || ((i_prawy <= i_koniec) && (tablica_glowna[i_lewy] > tablica_glowna[i_prawy]))))
             {
@@ -57,10 +55,9 @@
         }
     }
 
-
-//int main()
-//{
-   /* int i;
+int main()
+{
+    int i;
 
     cout <<"Przed sortowaniem:\n\n";
 
@@ -68,7 +65,7 @@
 
     for(i = 0; i < ile_liczb; i++) tablica_glowna[i] = rand() % 100;
     for(i = 0; i < ile_liczb; i++) cout << setw(4) << tablica_glowna[i];
-    cout << endl;
+    cout << endl<<endl;
 
     MergeSort(0,ile_liczb-1);
 
@@ -76,9 +73,10 @@
     for(i = 0; i < ile_liczb; i++) cout << setw(4) << tablica_glowna[i];
     cout << endl;
     return 0;
-}    */
+}
 
-    int main()
+
+   /* int main()
     {
         int i;
         int liczba=0;
@@ -88,17 +86,17 @@
         for (int j =0; j<5; j++)
 
         {
-            cout<< "wpisz " << j+1 <<" liczbe i nacisnij enter"<<endl;
+            cout<< "wpisz " << j+1 <<" liczbe do posortowania i nacisnij enter"<<endl;
             cin>>liczba;
-
+            tablica_glowna[j]=liczba;
         }
 
-        tablica_glowna[i]=liczba;
+        cout<<"Twoje liczby to: "<<endl;
 
 
         for(i = 0; i < 5; i++)
         {
-            cout <<tablica_glowna[i] <<endl;
+            cout <<tablica_glowna[i];
         }
 
 // Sortujemy
@@ -114,7 +112,7 @@
         return 0;
     }
 
-
+*/
 
 
 
